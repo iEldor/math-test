771a656474ea8f2046c599b9175c424478cebf2b
iEldor <ieldor@gmail.com>
2019-11-16 13:45:34 -0500

    timeout changes

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from random import randint
+from threading import Timer
+from pythoncom import CoInitialize
 from common import *
 import time
 
-number_of_questions = 15
+number_of_questions = 10
 timeout = 10
 assessment = []
+timed_out = False
+
+
+def set_timed_out():
+    CoInitialize()
+    global timed_out
+    timed_out = True
+    question['user_answer'] = None
+    question['score'] = 'TIMED OUT'
+    assessment.append(question)
+
+    print('Sorry, but time ran out for this question. Please continue...')
+    time.sleep(2)
+    shell_send_keys('{ENTER}')
+
 
 for i in range(1, number_of_questions + 1):
     factor_1 = randint(0,12)
@@ -17,13 +34,20 @@ for i in range(1, number_of_questions + 1):
         'answer': factor_1 * factor_2
     }
 
-    while True:
+    timed_out = False
+    while not timed_out:
+        clear_screen()
+        print_title('multiplications test'.upper())
+        print(f'You are answering the question {i} out of {number_of_questions}\n')
+        t = Timer(timeout, set_timed_out)
+        t.start()
+        user_answer = input(f"{question['question']}")
         try:
-            clear_screen()
-            print_title('multiplications test'.upper())
-            print(f'You are answering the question {i} out of {number_of_questions}\n')
-            user_answer = input(f"{question['question']}")
             user_answer = int(user_answer)
+            t.cancel()
+        except ValueError:
+            continue
+        else:
             question['user_answer'] = user_answer
             if question['answer'] == question['user_answer']:
                 question['score'] = 'PASS'
@@ -35,16 +59,17 @@ for i in range(1, number_of_questions + 1):
                 time.sleep(1)
             assessment.append(question)
             break
-        except ValueError:
-            print(f'[{user_answer}] is not a valid entry. Please try again ')
-            continue
+
 correct_answers = len([question['score'] for question in assessment if question['score'] == 'PASS'])
 incorrect_answers = len([question['score'] for question in assessment if question['score'] == 'FAIL'])
+timedout_questions = len([question['score'] for question in assessment if question['score'] == 'TIMED OUT'])
+
 print(f'''
-        Total Questions: {number_of_questions}
-        Correct Answers: {correct_answers}
-        Incorrec Answers: {incorrect_answers}
-        Score: {100*correct_answers/number_of_questions} %
+        Total Questions:    {number_of_questions}
+        Correct Answers:    {correct_answers}
+        Incorrect Answers:  {incorrect_answers}
+        Timed Out:          {timedout_questions}
+        Score:              {100*correct_answers/number_of_questions} %
 ''')
 
 for question in assessment:
--- common.py
+++ common.py
@@ -1,4 +1,5 @@
 import os
+from win32com.client import Dispatch
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -10,3 +11,8 @@ def print_title(text):
     print(' '*10 + text + ' '*10)
     print('_'*(len(text) + 20))
     print()    
+
+
+def shell_send_keys(text):
+    shell = Dispatch('WScript.Shell')
+    shell.SendKeys(text)
